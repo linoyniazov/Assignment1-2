ec2282f40996a0b037d6ad744d5d3711cda77117
Almogel <125140769+Almogel@users.noreply.github.com>
2024-12-14 14:31:09 +0200

    update comment

--- routes/comment.js
+++ routes/comment.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
     createComment,
     getComments,
+    updateComment,
 
 
 } = require('../controllers/comment');
 
 router.post('/', createComment);
 router.get('/:postId?', getComments);
+router.put('/:id', updateComment);
 
 
 
