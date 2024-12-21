3dd56656d72ff3ef25e40b86b28a07f1fae9d5ac
linoyniazov <linoy2209@gmail.com>
2024-12-21 17:19:43 +0200

    add tests JEST

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
+const appInit = require("./server");
 const port = process.env.PORT;
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECT);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-    console.log("Connected to the database");
-});
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const postRoutes = require("./routes/post"); 
-app.use("/post", postRoutes);
-
-const commentRouter = require('./routes/comment');
-app.use('/comments', commentRouter);
-
-app.listen(port, () => {
+const tmpFunc = async () => {
+  const app = await appInit();
+  app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});
+  });
+};
+tmpFunc();
